4b094943e2fd343d0313dceba5504ca90530960d
codr4life <codr4life@gmail.com>
2017-01-10 09:45:30 +0100

    extracted run-test

--- test.lisp
+++ test.lisp
@@ -19,6 +19,18 @@
 (defun untest (tags &key (suite *suite*))
   (slist-rem suite tags nil))
 
+
+(defgeneric run-test (tags fn &key warmup reps)
+  (:method (tags fn &key (warmup 0) (reps 1))
+    (format t "testing ~30a" tags)
+    (dotimes (_ warmup) (funcall fn))
+    (let ((time (get-internal-run-time)))
+      (dotimes (_ reps) (funcall fn))
+      (setf time (- (get-internal-run-time) time))
+      (format t "~5f~%"
+              (/ time internal-time-units-per-second))
+      time)))
+
 (defgeneric run-tests (tags &key)  
   (:method (tags &key (warmup 0) (reps 1)
                       skip
@@ -31,13 +43,10 @@
                          (null (set-difference tags test-tags)))
                      (or (null skip)
                          (not (intersection test-tags skip))))
-            (format t "testing ~30a" test-tags)
-            (dotimes (_ warmup) (funcall test-fn))
-            (let ((time (get-internal-run-time)))
-              (dotimes (_ reps) (funcall test-fn))
-              (setf time (- (get-internal-run-time) time))
-              (format t "~5f~%"
-                      (/ time internal-time-units-per-second))
-              (incf tot-time time)))))
+            
+            (incf tot-time
+                  (run-test test-tags test-fn
+                            :warmup warmup
+                            :reps reps)))))
       (format t "TOTAL ~32a~5f~%" ""
               (/ tot-time internal-time-units-per-second)))))
