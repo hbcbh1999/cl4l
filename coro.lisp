72b1acd3fa6ca1819b21812c8b31890373ea83f6
codr4life <codr4life@gmail.com>
2017-01-13 23:25:28 +0100

    coro fixes

--- coro.lisp
+++ coro.lisp
@@ -1,19 +1,16 @@
 (defpackage cl4l-coro
-  (:export coro-do coro-resume coro-return)
+  (:export do-coro coro-resume coro-return)
   (:shadowing-import-from cl4l-utils with-symbols)
   (:use cl cl4l-test))
 
 (in-package cl4l-coro)
 
-(define-condition coro-return (condition)
-  ((result :initarg :result :reader result)))
-
-(defmacro coro-return (result)
+(defmacro coro-return (&optional result)
   `(restart-case 
-       (error 'coro-return :result ,result)
+       (signal 'coro-return :result ,result)
      (coro-resume ())))
 
-(defmacro coro-do ((var expr) &body body)
+(defmacro do-coro ((var expr) &body body)
   (with-symbols (_c)
     `(handler-bind ((coro-return
                       (lambda (,_c)
@@ -22,11 +19,14 @@
                         (invoke-restart 'coro-resume))))
        ,expr)))
 
+(define-condition coro-return (condition)
+  ((result :initarg :result :reader result)))
+
 (define-test (:coro)
   (flet ((foo (max)
            (dotimes (i max)
              (coro-return i))))
     (let ((sum 0))
-      (coro-do (i (foo 10))
+      (do-coro (i (foo 10))
         (incf sum i))
       (assert (= 45 sum)))))
