f8f3d002aeede5f96fa298e48b196409d084efb2
codr4life <codr4life@gmail.com>
2017-01-15 01:52:53 +0100

    added critter macro

--- critter.lisp
+++ critter.lisp
@@ -1,38 +1,45 @@
 (defpackage cl4l-critter
-  (:export critter-next critter-yield do-critter with-critter)
+  (:export critter critter-next critter-yield with-critter)
   (:shadowing-import-from cl4l-utils symbol! with-symbols)
   (:shadowing-import-from cl-cont lambda/cc)
   (:use cl cl4l-test))
 
 (in-package cl4l-critter)
 
-(defmacro critter-yield (cont &optional result)
-  (with-symbols (_cont)
-    `(cl-cont:let/cc ,_cont
-       (setf ,cont ,_cont)
+(defmacro critter ((context args) &body body)
+  `(lambda/cc ,args
+     (macrolet ((critter-yield (&optional result)
+                  (let ((_context ',context))
+                    `(cl-cont:let/cc c
+                       (setf ,_context c)
+                       ,result))))
+       ,@body)))
+
+(defmacro critter-yield (context &optional result)
+  (with-symbols (_context)
+    `(cl-cont:let/cc ,_context
+       (setf ,context ,_context)
        ,result)))
 
-(defmacro do-critter ((cont expr) &body body)
+(defmacro with-critter ((context expr) &body body)
   ;; Executes BODY with EXPR bound to optional NAME,
   ;; hard coded synonyms are provided for anonymous use.
   `(let ((critter-result ,expr))
      (macrolet ((critter-next ()
-                  (let ((cn ',cont))
-                    `(setf critter-result (funcall ,cn)))))
+                  (let ((_context ',context))
+                    `(setf critter-result (funcall ,_context)))))
        ,@body)))
-
+  
 (defparameter test-max 100000)
 
-;; todo add with-critter macro to encapsulate lambda/cc && cont
-
 (define-test (:critter)
-  (let* ((cont)
-         (foo (lambda/cc (max)
+  (let* ((context)
+         (foo (critter (context (max))
                 (dotimes (i max)
-                  (critter-yield cont i))))
+                  (critter-yield i))))
          (j 0))
-    (do-critter (cont (funcall foo test-max))
-        (assert (= j critter-result))
+    (with-critter (context (funcall foo test-max))
+      (assert (= j critter-result))
       (incf j)
       (critter-next))))
 
